9851880773733dad596f60607d67eacb1725d197
yongsuk204 <yongsuk204@gmail.com>
2025-07-24 16:38:11 +0900

    Initial Commit

---
+++ testRunning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testRunning
+//
+//  Created by 배용석 on 7/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testRunning/testRunningApp.swift
@@ -0,0 +1,17 @@
+//
+//  testRunningApp.swift
+//  testRunning
+//
+//  Created by 배용석 on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct testRunningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
